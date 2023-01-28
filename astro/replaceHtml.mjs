b7acb18136d6a728af3a3b36ae714cd46c67a919
NomuraKohei <seiryu2nd@gmail.com>
2023-01-28 15:37:26 +0900

    fix: アロー関数へ

--- astro/replaceHtml.mjs
+++ astro/replaceHtml.mjs
@@ -1,19 +1,25 @@
-import { readFile, writeFile } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import glob from 'glob';
 
-glob('dist/**/*.html', function (er, files) {
-  files.forEach(function (file) {
-    readFile(file, 'utf8', function (err, data) {
-      if (err) throw err;
+const replaceInHtmlFiles = () => {
+  try {
+    const files = glob.sync('dist/**/*.html');
+    for (const file of files) {
+      // htmlファイルの読み込み
+      const data = readFileSync(file, 'utf8');
+      // htmlの置かれているパスから相対(., ..)を算出
       const relativePath = file.replace(/[^/]/g, '').replace(/\//g, '.');
+      // href, srcに指定されている絶対パスを置換
       const result = data
         .replace(/\/~partytown\//g, `./~partytown/`)
         .replace(/href=\//g, `href=${relativePath}/`)
         .replace(/src=\//g, `src=${relativePath}/`)
         .replace(/(<a.*?href.*?\/)(?=\s>)/g, '$1index.html');
-      writeFile(file, result, 'utf8', function (err) {
-        if (err) return console.log(err);
-      });
-    });
-  });
-});
+      writeFileSync(file, result, 'utf8');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+replaceInHtmlFiles();
