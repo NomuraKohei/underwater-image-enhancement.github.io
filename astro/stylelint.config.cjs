5bc2e0725cc2fa39ec4a6d81160b6ab0be020ef0
NomuraKohei <seiryu2nd@gmail.com>
2023-01-28 15:44:58 +0900

    fix: directry

--- astro/stylelint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Put `stylelint-config-prettier` at the end of the extends array
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-    'stylelint-config-html',
-    'stylelint-config-prettier',
-  ],
-};
